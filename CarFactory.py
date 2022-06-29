1b5e0f86ffb0605633b09e9318c28733a161d2de
Ayush-Noorani <84242031+Ayush-Noorani@users.noreply.github.com>
2022-06-29 12:53:44 +0530

    Add files via upload

---
+++ CarFactory.py
@@ -0,0 +1,22 @@
+from battery.Battery import Battery
+from car import Car
+from engine.Engine import Engine
+
+class CarFactory():
+    c = Car()
+    e = Engine()
+    b = Battery()
+    def create_calliope(current_date,last_service_date,current_milage,last_service_milage):
+        pass
+
+    def create_glissade(current_date,last_service_date,current_milage,last_service_milage):
+        pass
+
+    def create_palindrome(current_date,last_service_date,warning_light_on):
+        pass
+
+    def create_rorschach(current_date,last_service_date,current_milage,last_service_milage):
+        pass
+
+    def create_thovex(current_date,last_service_date,current_milage,last_service_milage):
+        pass
